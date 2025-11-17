7ed601471077c6368721930f78d00dfcf7d8ba7b
Happyface9698 <nathan.magana@gctcok.edu>
2025-11-17 12:23:14 -0600

    finished

--- Casting_Magana_Natahan/Program.cs
+++ Casting_Magana_Natahan/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please tell me your favorite number: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Your favorite number is: " + userInput);
+
+            Console.WriteLine("True or False, Programmers some times wish they had an extra finger!");
+            bool isTrue = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("It is " + isTrue);
         }
     }
 }
