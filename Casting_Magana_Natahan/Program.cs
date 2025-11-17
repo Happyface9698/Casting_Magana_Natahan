876756b2f7ad936b7c7a725dac6a99a353f71c8d
Happyface9698 <nathan.magana@gctcok.edu>
2025-11-17 14:45:13 -0600

    3rdrun

--- Casting_Magana_Natahan/Program.cs
+++ Casting_Magana_Natahan/Program.cs
@@ -3,14 +3,14 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Please tell me your favorite number: ");
-            string userInput = Console.ReadLine();
-            Console.WriteLine("Your favorite number is: " + userInput);
+        {   
+            Console.WriteLine("Please tell me your favorite number: "); // Prompt user for favorite number
+            string userInput = Console.ReadLine();// Read user input as string
+            Console.WriteLine("Your favorite number is: " + userInput); // Display the favorite number
 
-            Console.WriteLine("True or False, Programmers some times wish they had an extra finger!");
-            bool isTrue = Convert.ToBoolean(Console.ReadLine());
-            Console.WriteLine("It is " + isTrue + " sometimes.");
+            Console.WriteLine("True or False, Programmers some times wish they had an extra finger!"); // Prompt user for a boolean input
+            bool isTrue = Convert.ToBoolean(Console.ReadLine()); // Read user input and convert to boolean
+            Console.WriteLine("It is " + isTrue + " sometimes."); // Display the boolean value
 
         }
     }
