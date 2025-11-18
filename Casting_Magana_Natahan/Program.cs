31ee48b786f2a627b8642ab1a45e9e33a90cea3b
Happyface9698 <nathan.magana@gctcok.edu>
2025-11-18 10:28:00 -0600

    RAWR

--- Casting_Magana_Natahan/Program.cs
+++ Casting_Magana_Natahan/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {   
             Console.WriteLine("Please tell me your favorite number: "); // Prompt user for favorite number
-            string userInput = Console.ReadLine();// Read user input as string
+            int userInput = Convert.ToInt32(Console.ReadLine());// Read user input as string
             Console.WriteLine("Your favorite number is: " + userInput); // Display the favorite number
 
             Console.WriteLine("True or False, Programmers some times wish they had an extra finger!"); // Prompt user for a boolean input
